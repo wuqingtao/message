30a2048eaf7eb4a014181f82d51d4329735c0ab5
wuqingtao <qingtao.wu@live.cn>
2016-11-13 21:04:08 +0800

    modified:   deploy_html.sh 	deleted:    start_server.sh 	deleted:    stop_server.sh 	modified:   todo.md 	added:	deploy_server.sh 	sdded:	server/start_server.sh 	added:	server/stop_server.sh

--- deploy_html.sh
+++ deploy_html.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-rm -rf /usr/local/nginx/html/*
-mkdir /usr/local/nginx/html/message
-path=`pwd`
-cp -r $path/html/* /usr/local/nginx/html/message/
+local_path=`dirname $0`/html
+target_path=/usr/local/nginx/html/message
+rm -rf $target_path
+mkdir $target_path
+cp -r $local_path/* $target_path
