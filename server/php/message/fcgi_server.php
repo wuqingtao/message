1142e85f1a2c3ffb2e66ee0e8cadeff621f5dd24
wuqingtao <qingtao.wu@live.cn>
2016-11-24 00:04:10 +0800

    php版本实现日志

--- server/php/message/fcgi_server.php
+++ server/php/message/fcgi_server.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once('../utils/log4php/Logger.php');
+
+date_default_timezone_set('PRC');
+Logger::configure(getcwd().'/../utils/log4php/Logger.properties');
+$logger = Logger::getLogger('server');
+
 require_once('message_creator.php');
 
 $user = 'root';
@@ -16,6 +22,7 @@ if ($method == 'POST') {
 	$res = $message->request($param);
 	$content = json_encode($res);
 	
+	$logger->info($content);
 	file_put_contents("php://output", $content);
 } else {
 	http_response_code(501);
